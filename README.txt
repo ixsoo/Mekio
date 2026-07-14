f38fd97cc92cfdf5548ca0b9a0f594c48d2471eb
ixsoo <mikael.isokaanta@telia.com>
2026-07-14 20:09:59 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,9 +1,18 @@
-MEKIO – testapp v0.2
+MEKIO BETA v0.3
 
-Fixat:
-- Appen går nu vidare från startskärmen.
-- JavaScript-felet i Garageläget är rättat.
-- Cachen uppdateras automatiskt till v0.2.
-- Startanimationen har en robust reservlösning.
+NYTT:
+- Mer centrerad startsida
+- Tillbaka-knappar på undersidor
+- OKS195 visar Volvo 240
+- JXD416 visar Volvo V90
+- Sök-animation: “Volvo 240 hittad” / “Volvo V90 hittad”
+- Favoritstjärna med pop-animation
+- Gröna bekräftelser
+- “Välkommen tillbaka, Mikael”
+- Sparade bilar visas som kort
+- Garageläget centrerat bättre
+- Ny Mekio-tip-ruta som liten överraskning
+- Ny cacheversion så uppdateringen laddas in
 
-Ladda upp samtliga filer till GitHub och ersätt filer med samma namn.
+UPPDATERING PÅ GITHUB:
+Ladda upp samtliga filer och ersätt filer med samma namn.
--- sw.js
+++ sw.js
@@ -1,27 +1,25 @@
-const CACHE='mekio-v02';
+const CACHE='mekio-v03';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', e => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
+self.addEventListener('activate', e => {
+  e.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return r;
       })
-      .catch(() => caches.match(event.request))
+      .catch(() => caches.match(e.request))
   );
 });
